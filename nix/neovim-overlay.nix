3fed6832cbf4389e43eef0072523134468855baa
Bigtruck2 <86583611+Bigtruck2@users.noreply.github.com>
2024-05-01 23:18:00 -0400

    add languagetool

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -78,12 +78,14 @@ with final.pkgs.lib; let
     plenary-nvim
     nvim-web-devicons
     vim-repeat
+    coc-ltex
     # ^ libraries that other plugins depend on
     # bleeding-edge plugins from flake inputs
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
   ];
+
 
   extraPackages = with pkgs; [
     # language servers, etc.
