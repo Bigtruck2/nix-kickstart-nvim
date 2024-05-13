4a1fd531c33c702b1bf3ba3aa733d048dc270d0b
Bigtruck2 <86583611+Bigtruck2@users.noreply.github.com>
2024-05-13 10:47:16 -0400

    add tabby

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -87,6 +87,7 @@ with final.pkgs.lib; let
     # bleeding-edge plugins from flake inputs
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
+    tabby-nvim
     which-key-nvim
   ];
 
