43fd430b240eab81a47abec6859565e29e31594a
Sebastian Benatar <bigtruck2e@gmail.com>
2024-07-19 12:42:52 -0400

    add keymapings

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -14,6 +14,17 @@ harpoon:setup({})
 
 -- basic telescope configuration
 local conf = require("telescope.config").values
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-'>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 local function toggle_telescope(harpoon_files)
     local file_paths = {}
     for _, item in ipairs(harpoon_files.items) do
